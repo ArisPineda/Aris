5d031cc309860506c5ec711e698e3a06fa04045c
ArisPineda <423001711@ntc.edu.ph>
2024-05-31 15:05:06 +0800

    Update solidity.sol

--- solidity.sol
+++ solidity.sol
@@ -1,26 +1,22 @@
-
+pragma solidity ^0.8.0;
 contract MyToken {
-    string public name;
-    string public symbol;
-    uint256 public totalSupply;
-
-    mapping(address => uint256) public balances;
-
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) {
-        name = _name;
-        symbol = _symbol;
-        totalSupply = _initialSupply;
-        balances[msg.sender] = _initialSupply;
+   
+    string public tokenName;
+    string public tokenAbbreviation;
+    uint public totalSupply;
+    
+    mapping(address => uint) public balances;
+    
+    
+    function mint(address _recipient, uint _value) public {
+        totalSupply += _value; 
+        balances[_recipient] += _value; 
     }
 
-    function mint(address _receiver, uint256 _value) public {
-        totalSupply += _value;
-        balances[_receiver] += _value;
-    }
-
-    function burn(address _owner, uint256 _value) public {
-        require(balances[_owner] >= _value, "Insufficient balance");
-        totalSupply -= _value;
-        balances[_owner] -= _value;
+   
+    function burn(address _owner, uint _value) public {
+        require(balances[_owner] >= _value, "Insufficient balance"); 
+        totalSupply -= _value; 
+        balances[_owner] -= _value; 
     }
 }
